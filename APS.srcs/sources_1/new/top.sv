6498648f8695492cd534d9201827cfb76807e2ac
Максимов Артем Владимирович <8200464@sipc.miet.ru>
2022-09-12 15:24:45 +0300

    added basic

--- APS.srcs/sources_1/new/top.sv
+++ APS.srcs/sources_1/new/top.sv
@@ -1,16 +1,42 @@
 `timescale 1ns / 1ps
 
 module top(
-    input       clk,
-    input       a,
-    input       b,
-    output reg  q   );
+    input [15:0] SW,
+    input [15:0] LED
+    );
+    
+assign LED[0] = SW[0] & SW[1];
+assign LED[2] = SW[2] | SW[3];
+assign LED[4] = SW[4] ^ SW[5];
+assign LED[10:6] = ~SW[10:6];
+assign LED[13:11] = {SW[11], SW[12], SW[13]};
+assign LED[15:14] = { 2{SW[14]} };
 
-wire c;
+//    input       clk,
+//    input       a,
+//    input       b,
+//    output reg  q   );
 
-assign c = a ^ b;
+//wire c;
 
-always @(posedge clk) begin
-    q <= c;
-end
+//assign c = a ^ b;
+
+//always @(posedge clk) begin
+//    q <= c;
+//end
 endmodule
+
+
+//module basic(
+//    input [15:0] SW,
+//    input [15:0] LED
+//    );
+    
+//assign LED[0] = SW[0] & SW[1];
+//assign LED[2] = SW[2] | SW[3];
+//assign LED[4] = SW[4] ^ SW[5];
+//assign LED[10:6] = ~SW[10:6];
+//assign LED[13:11] = {SW[11], SW[12], SW[13]};
+//assign LED[15:14] = { 2{SW[14]} };
+
+//endmodule 
